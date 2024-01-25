2c6a9209e44f57a2882cfce72cbc54132daddefb
Marina Yarosh <yaroshmv@yandex.ru>
2024-01-25 11:34:44 +0300

    add seminar1ask3

--- seminar1/Program.cs
+++ seminar1/Program.cs
@@ -1,11 +1,12 @@
-﻿int N=4;
+﻿// Task2
+int N=4;
 if (N>0)
 {
     int i=-N;
     while (i<=N)
     {
-        System.Console.Write(i);
-        System.Console.Write(',');
+       // System.Console.Write(i);
+       // System.Console.Write(',');
         i+=1;
     }
 }
@@ -14,8 +15,17 @@ else
     int i=N;
     while (i<=-N)
     {
-        System.Console.Write(i);
-        System.Console.Write(',');
+        // System.Console.Write(i);
+        // System.Console.Write(',');
         i+=1;
     }
 }
+
+// Task3
+int a = 782;
+
+int a1 = a / 100;
+int a2 = a % 10;
+
+int sum = a1 + a2;
+System.Console.WriteLine(sum);
