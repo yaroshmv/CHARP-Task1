42761c7c97b3b19ade445ca1b0784ccdb956d4d1
Marina Yarosh <yaroshmv@yandex.ru>
2024-01-25 11:26:30 +0300

    add seminat1task2

---
+++ seminar1/Program.cs
@@ -0,0 +1,21 @@
+﻿int N=4;
+if (N>0)
+{
+    int i=-N;
+    while (i<=N)
+    {
+        System.Console.Write(i);
+        System.Console.Write(',');
+        i+=1;
+    }
+}
+else
+{
+    int i=N;
+    while (i<=-N)
+    {
+        System.Console.Write(i);
+        System.Console.Write(',');
+        i+=1;
+    }
+}
