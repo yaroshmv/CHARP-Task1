4ee51b2240ecbbfead32506e9dcd6bffd2deb1ea
Marina Yarosh <yaroshmv@yandex.ru>
2024-01-26 14:16:10 +0300

    add lesson2task1

---
+++ Lesson2/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿int [] arr = new int [5];
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 5;
+arr[3] = 4;
+arr[4] = 3;
+System.Console.WriteLine(arr[0]);
+System.Console.WriteLine(arr[1]);
+System.Console.WriteLine(arr[2]);
+System.Console.WriteLine(arr[3]);
+System.Console.WriteLine(arr[4]);
+System.Console.WriteLine(arr[5]);
