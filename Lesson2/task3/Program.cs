bff01ddf656facbed1463b7573b362b970924b8b
Marina Yarosh <yaroshmv@yandex.ru>
2024-01-26 16:05:14 +0300

    add lesson2task3

---
+++ Lesson2/task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//int n = 10;
+int[] array = { 3, 43, 25, 1, 23, 11, 17, 7, 49, 5 };
+int i = 0;
+
+while (i < array.Length)
+{
+    if (array[i] % 2 == 0)
+    //Console.Write($"{array[i]} ");
+    System.Console.Write(array[i]);
+    System.Console.Write(' ');
+    i = i + 1;
+}
