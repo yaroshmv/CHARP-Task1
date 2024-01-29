ed44708f12cff0a9ccc116b6a462e7a98987ade5
Marina Yarosh <yaroshmv@yandex.ru>
2024-01-29 21:07:38 +0300

    add seminar2task2

---
+++ Hometask/seminar2task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int x = 4;
+int y = -2;
+
+
+if (x > 0 & y > 0)
+{
+    System.Console.WriteLine(1);
+}
+else if (x < 0 & y > 0)
+{
+    System.Console.WriteLine(2);
+}
+else if (x < 0 & y < 0)
+{
+    System.Console.WriteLine(3);
+}
+else if (x > 0 & y < 0)
+{
+    System.Console.WriteLine(4);
+}
