75db8fa1e6be578eaaa07cb2075d324fca419a40
Marina Yarosh <yaroshmv@yandex.ru>
2024-01-30 14:50:37 +0300

    ad seminar2task3

---
+++ Hometask/seminar2task3/Program.cs
@@ -0,0 +1,16 @@
+﻿int n = 96;
+
+if (n >= 10 & n <= 99)
+{
+    int n1 = (n % 10);
+    int n2 = (n / 10); 
+
+    if (n1 > n2)
+    {
+        System.Console.WriteLine(n1);
+    }
+    else
+    {
+        System.Console.WriteLine(n2);
+    }
+}
