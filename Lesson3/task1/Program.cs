159dda62274ff76a76b73b4c470bda95e9017a79
Marina Yarosh <yaroshmv@yandex.ru>
2024-01-30 19:50:21 +0300

    add lesson3lask1

---
+++ Lesson3/task1/Program.cs
@@ -0,0 +1,9 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double nomenator = a * b;
+    int denomenator = a + b;
+    double result = nomenator / denomenator;
+    return result;
+}
+double result = CalculateFormula(1, 2, 3, 4);
+System.Console.WriteLine(result);
