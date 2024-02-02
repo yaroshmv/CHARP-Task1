90ee742942f33b95928fadfe82210c3e0fd385ca
Marina Yarosh <yaroshmv@yandex.ru>
2024-02-02 18:00:19 +0300

    add task3

---
+++ seminar3/task3/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 2, 3, 1, 7, 5, 6, 3 };
+int n = array.Length - 1;
+int[] result = new int[array.Length / 2];
+
+for (int i = 0; i < n - i; i++)
+{
+    result[i] = array[i] * array[n - i];
+}
+
+for (int i = 0; i < result.Length; i++)
+{
+    System.Console.Write(result[i] + " ");
+}
