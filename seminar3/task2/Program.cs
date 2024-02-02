02829c28d5c2a4fa3e4e0be38fa54e609fecfdab
Marina Yarosh <yaroshmv@yandex.ru>
2024-02-02 17:29:42 +0300

    add task2

---
+++ seminar3/task2/Program.cs
@@ -0,0 +1,16 @@
+﻿const int size = 10;
+int[] array = new int[size] { 1, -5, 6, 5, -7, 8, 2, -2, -3, 9 };
+
+for (int i = 0; i < size; i++)
+{
+    System.Console.Write($"{array[i]} ");
+}
+System.Console.WriteLine();
+for (int i = 0; i < size; i++)
+{
+    array[i] = -array[i];
+}
+for (int i = 0; i < size; i++)
+{
+     System.Console.Write($"{array[i]} ");
+}
