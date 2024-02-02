5ff2589c2cd6b72da79eda5b56ca86f34a10b9fa
Marina Yarosh <yaroshmv@yandex.ru>
2024-02-02 16:44:26 +0300

    add seminar3task1

---
+++ seminar3/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 34, 19, 3 };
+int number = 8;
+bool result = false;
+for (int i = 0; i < array.Length; i = i + 1)
+{
+    if (array[i] == number)
+        result = true;
+}
+if (result == true)
+{
+    System.Console.WriteLine("Yes");
+}
+else
+{
+    System.Console.WriteLine("No");
+}
