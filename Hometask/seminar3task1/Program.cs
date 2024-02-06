3ec1af4d82b82055841316e612cc639d57d2b4ac
Marina Yarosh <yaroshmv@yandex.ru>
2024-02-06 21:12:17 +0300

    add seminar3task1

---
+++ Hometask/seminar3task1/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] numbers = new int[] { 1, 5, 10, 20, 30, 40, 99, 4, 90, 3 };
+int count = 0;
+foreach (int number in numbers)
+{
+    if (number >= 10 && number <= 90)
+        return count++;
+}
+Console.WriteLine ("Количество элементов массива, значения которых лежат в отрезке от 30 до 60: " + count);
