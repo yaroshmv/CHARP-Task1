4ecdbcefe35f0eff695ab48483ac70aa600b17d8
Marina Yarosh <yaroshmv@yandex.ru>
2024-02-18 07:15:00 +0300

    add lessom4task1

---
+++ lesson4/task1/Program.cs
@@ -0,0 +1,50 @@
+﻿//Создать двумерный массив с размерами 3х5, состоящий из целых чисел. Вывести его на экран.
+//1.Указать тип данных
+//2.Указать имя массива
+//3.Выделить память под элементы
+int[,] matrix = new int[4, 5];
+Random rnd = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = rnd.Next(1, 11);
+    }
+}
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        System.Console.Write($"{matrix[i, j]} ");
+    }
+    System.Console.WriteLine();
+}
+
+int[,] CreateMatrix(int rowCount, int columnsCount)
+{
+    int[,] matrix = new int[rowCount, columnsCount];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] mat = CreateMatrix(3, 3);
+ShowMatrix(matrix);
