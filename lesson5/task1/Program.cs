0c6979f73a2187fab16b274cc17660d93bccbc76
Marina Yarosh <yaroshmv@yandex.ru>
2024-02-19 03:22:15 +0300

    addlesson5 task1

---
+++ lesson5/task1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Вычисить факториал от натурального числа N
+int Fact(int n)
+{
+    if (n == 1)
+    {
+        System.Console.WriteLine($"Stop: {n}");
+        return 1;
+    }
+    System.Console.WriteLine(n);
+    return n * Fact(n - 1);
+}
+System.Console.WriteLine(Fact(5));
+
+//F11 Шаг с заходом
+//F10 Шаг с обходом
