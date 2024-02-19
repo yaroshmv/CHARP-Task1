c3e4f8f3e9adae3b1d6a1796ab59226cc638b9be
Marina Yarosh <yaroshmv@yandex.ru>
2024-02-19 03:51:50 +0300

    add task1

---
+++ Hometask/finalhometask/task1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+void PrintRange(int n, int m = 4)
+{
+    if (m > n)
+    {
+        System.Console.WriteLine();
+        return;
+    }
+    System.Console.Write($"{m} ");
+    PrintRange(n, m + 1);
+}
+PrintRange(8);
+PrintRange(4);
