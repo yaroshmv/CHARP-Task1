51c42b370db6f39e8243741fce1c2d50b333ec56
Marina Yarosh <yaroshmv@yandex.ru>
2024-02-19 04:15:17 +0300

    add task2

---
+++ Hometask/finalhometask/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+int m = 2;
+int n = 3;
+int functionAkkerman = Ack(m, n);
+
+Console.Write(functionAkkerman);
+
+int Ack(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Ack(m - 1, 1);
+    else return Ack(m - 1, Ack(m, n - 1));
+}
